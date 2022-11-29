f6502c75818df2b1fd47198ef04c931255dede0c
Philippe LERCH <philippe.lerch68@gmail.com>
2022-11-29 21:52:14 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+# Configuration
+
+url = "https://analyticsengineeringprojects.s3.eu-west-1.amazonaws.com/AE_ChessAnalytics.7z"
+save_as = "AE_ChessAnalytics.7z"
+
---
+++ importdata.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+import py7zr
+ 
+from config import *
+
+
+def download():
+    #url = "https://analyticsengineeringprojects.s3.eu-west-1.amazonaws.com/AE_ChessAnalytics.7z"
+    #save_as = "AE_ChessAnalytics.7z"
+    print(f"Downloading {url}")
+    # Download from URL
+    with urlopen(url) as file:
+        content = file.read()
+
+    # Save to file
+    print(f"saving {save_as}")
+    with open(save_as, 'wb') as download:
+        download.write(content)
+
+def extract():
+    with py7zr.SevenZipFile("AE_ChessAnalytics.7z", 'r') as archive:
+        archive.extractall(path="./data/")
+
+if __name__=='__main__':
+    print(" starting")
+    download()
+    print(" done ")
+    print("extract data")
+    extract()
+    print("end extraction")
