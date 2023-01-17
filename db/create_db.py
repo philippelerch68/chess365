07543b0a9c388352c26d622e266c9e440e3022ba
cuchri <cuchri_dev@outlook.com>
2023-01-17 21:48:09 +0100

    implement function definition

--- db/create_db.py
+++ db/create_db.py
@@ -1,29 +1,32 @@
 import mysql.connector
 from mysql.connector import Error
-from config import *
+from config import host, database, user, password
 
 
-#create database
-try:
-    connection = mysql.connector.connect(
-    host=db_host,
-    user=db_user,
-    password=db_password
-    )
+def create_database(host=host, database=database, user=user, password=password):
+    """DDL step; creates the database
 
-    cursor = connection.cursor()
-    cursor.execute(f"DROP DATABASE IF EXISTS {db_database}")
-    cursor.execute(f"CREATE DATABASE {db_database}")
-    print(f"Database {db_database} created successfully")
+    Args:
+        host (str): ip_address. Defaults to host defined in config.
+        database (str): name of the database. Defaults to database defined in config.
+        user (str): database user for connection. Defaults to user defined in config.
+        password (str): password of the user. Defaults to password defined in config.
+    """
 
-except Error as error:
-    print(f"Failed to create database in MySQL: {error}")
-finally:
-    if connection.is_connected():
-        print("The following databases are available:")
-        cursor.execute("SHOW DATABASES")
-        for x in cursor:
-            print(x) 
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+    try:
+        connection = mysql.connector.connect(host=host,
+                                            user=user,
+                                            password=password)
+
+        cursor = connection.cursor()
+        cursor.execute(f"DROP DATABASE IF EXISTS {db_database}")
+        cursor.execute(f"CREATE DATABASE {db_database}")
+        print(f"Database {db_database} created successfully")
+
+    except Error as error:
+        print(f"Failed to create database {db_database}: {error}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
