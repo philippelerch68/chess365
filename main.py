47e4b1ea78079387b3c0be6d9ea1a90866974fe2
cuchri <cuchri_dev@outlook.com>
2023-01-17 22:11:19 +0100

    add ddl to main

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from config import *
-from importdata import *
-from gamesparser_to_db import *
-from playersparser_to_db import *
+from db.create_db import create_database
+from db.create_tables import create_tables
+from download.importdata import download, extract
+from download.gamesparser_to_db import games_parsing
+from download.playersparser_to_db import players_parsing
 
 
 
 if __name__=='__main__':
     
-    print("Starting                ", end='\r')
+    print("Starting process        ", end='\r')
+    print("----------------------------")
+    
+    print("Creating database       ", end='\r')
+    download()
+    print("Database created        ", end='\r')
+    
+    print("Creating tables         ", end='\r')
+    download()
+    print("Tables created          ", end='\r')
+    print("--------END OF DDL --------")
+    
     print("Loading compressed file ", end='\r')
     download()
     print("File downloaded         ", end='\r')
+    
     print("Extracting data         ", end='\r')
     extract()
-    print("End extraction          ", end='\r')
-          
-    print("----------------------------")
+    print("Data extracted          ", end='\r')
+    print("--------END OF EXTRACT --------")
+    
     print("IMPORTING folder games files to db")
     games_parsing()
     print("Games import done")
@@ -24,4 +38,4 @@ if __name__=='__main__':
     print("IMPORTING folder players files to db")
     players_parsing()
     print("Players import done")
-    print("--------END OF IMPORTATION --------")
+    print("--------END OF LOAD --------")
