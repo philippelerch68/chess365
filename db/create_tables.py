9e4559a3c6b6920cd3b153f9de9c7726602f7845
cuchri <cuchri_dev@outlook.com>
2023-01-17 21:43:49 +0100

    adopt docstring

---
+++ db/create_tables.py
@@ -0,0 +1,42 @@
+import mysql.connector
+from mysql.connector import Error
+from config import host, database, user, password
+from db_ddl import tables
+
+
+def create_tables(tables_dict = tables, host=host, database=database, user=user, password=password):
+    """DDL step; creates defined tables in database
+
+    Args:
+        tables_dict (dict): definition of tables. Defaults to tables.
+        host (str): ip_address. Defaults to host defined in config.
+        database (str): name of the database. Defaults to database defined in config.
+        user (str): database user for connection. Defaults to user defined in config.
+        password (str): password of the user. Defaults to password defined in config.
+    """
+
+    try:
+        connection = mysql.connector.connect(host=host,
+                                            database=database,
+                                            user=user,
+                                            password=password)
+
+        cursor = connection.cursor()
+        print(f"Connection to database {database} established successfully")
+        
+        for tab in tables.keys():
+            try: 
+                result0 = cursor.execute(f"DROP TABLE IF EXISTS {tab}")
+                result = cursor.execute(f"CREATE TABLE {tab} {tables.get(tab)}")
+                print(f"Table {tab} created successfully ")
+                
+            except mysql.connector.Error as error:
+                print("Failed to create table {tab}: {error}")
+
+    except mysql.connector.Error as error:
+        print(f"Failed to connect to database {database}: {error}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print(f"Connection to database {database} is closed")
