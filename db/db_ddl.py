56790ba561ef19f4fa968caced3680037d3d75ec
cuchri <cuchri_dev@outlook.com>
2023-01-25 19:46:49 +0100

    define transform dml

--- db/db_ddl.py
+++ db/db_ddl.py
@@ -15,23 +15,22 @@ tables = {
     
 	"games_raw": """(
         id INT(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        event VARCHAR(40) NULL,
-        site VARCHAR(30) NULL,
+        event VARCHAR(100) NULL,
+        site VARCHAR(100) NULL,
         date VARCHAR(12) NULL,
         round VARCHAR(6) NULL,
-        white VARCHAR(35) NULL,
-        black VARCHAR(35) NULL,
-        result varchar(10) NULL,
-        whiteelo VARCHAR(5) NULL,
-        blackelo VARCHAR(5) NULL,
-        eco VARCHAR(5) NULL,
-        game VARCHAR(2000) NULL
+        white VARCHAR(100) NULL,
+        black VARCHAR(100) NULL,
+        result varchar(20) NULL,
+        whiteelo VARCHAR(10) NULL,
+        blackelo VARCHAR(10) NULL,
+        eco VARCHAR(10) NULL,
+        game VARCHAR(5000) NULL
         )
     """,
     
     "player": """(
-        id INTEGER NOT NULL PRIMARY KEY,
-        name VARCHAR(100) NULL,
+        id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         firstname VARCHAR(100) NULL,
         lastname VARCHAR(100) NULL,
         birthyear INTEGER UNSIGNED NULL,
@@ -40,10 +39,10 @@ tables = {
     """,
     
     "playerdetails": """(
-        id INTEGER NOT NULL PRIMARY KEY,
+        id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         player_id INTEGER NOT NULL,
-        title_id INTEGER NOT NULL,
-        federation_id INTEGER NOT NULL,
+        title_id INTEGER NULL,
+        federation_id INTEGER NULL,
         ranking INTEGER UNSIGNED NULL,
         elo INTEGER UNSIGNED NULL,
         games INTEGER UNSIGNED NULL,
@@ -64,7 +63,7 @@ tables = {
     """,
     
     "event": """(
-        id INTEGER NOT NULL PRIMARY KEY,
+        id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         name VARCHAR(100),
         location_id INTEGER UNSIGNED NOT NULL
         )
@@ -72,13 +71,13 @@ tables = {
     
     "dim_location": """(
         id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        txt VARCHAR(10),
-        txt1 VARCHAR(10)
+        txt VARCHAR(100),
+        txt1 VARCHAR(100)
         )
     """,
     
     "game": """(
-        id INTEGER NOT NULL PRIMARY KEY,
+        id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         event_id INTEGER UNSIGNED NOT NULL,
         white_player_id INTEGER UNSIGNED NOT NULL,
         black_player_id INTEGER UNSIGNED NOT NULL,
@@ -87,7 +86,8 @@ tables = {
         gamedate DATE,
         stage VARCHAR(10),
         whiteelo INTEGER UNSIGNED,
-        blackelo INTEGER UNSIGNED
+        blackelo INTEGER UNSIGNED,
+        moves VARCHAR(5000)
         )
     """,
     
@@ -104,7 +104,7 @@ tables = {
     """,
     
     "moves": """(
-        id INTEGER NOT NULL PRIMARY KEY,
+        id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         game_id INTEGER NOT NULL,
         movenr INTEGER UNSIGNED,
         white VARCHAR(10),
--- transform/parse_datamodel.py
+++ transform/parse_datamodel.py
@@ -29,99 +29,120 @@ inserts_stmt = {
         )
     """,
     #player_data
-    #TODO
     "player": """
-        (id, name, firstname, lastname, birthyear, fideid)
-        SELECT id
-            , name
-            , name as firstname
-            , name as lastname
-            , birthyear, fideid
-        FROM players_raw
+        (firstname, lastname, birthyear, fideid)
+        SELECT gr1.firstname, gr1.lastname, pr.birthyear, pr.fideid
+        FROM (
+            SELECT DISTINCT
+                player_name
+                , SUBSTRING_INDEX(player_name,',',-1) AS firstname
+                , SUBSTRING_INDEX(player_name,',',1) AS lastname
+            FROM (
+                SELECT DISTINCT white AS player_name FROM games_raw
+                UNION ALL 
+                SELECT DISTINCT black AS player_name FROM games_raw
+            ) gr
+        ) gr1
+        LEFT JOIN players_raw pr
+            ON REPLACE(pr.name, "`", "'") = TRIM(CONCAT_WS(' ', gr1.firstname, gr1.lastname))
     """,
     
+    ##insert empty
     "dim_title": """
         (txt) 
-        SELECT DISTINCT (title) 
+        SELECT null AS txt 
+            UNION ALL
+        SELECT DISTINCT (title) AS txt
         FROM players_raw
     """,
     
     "dim_federation": """
         (txt) 
-        SELECT DISTINCT (federation) 
+        SELECT null AS txt 
+            UNION ALL
+        SELECT DISTINCT (federation) AS txt
         FROM players_raw
     """,
     
     "playerdetails": """
-        (id, player_id, title_id, federation_id, ranking, elo, games, page)
-        SELECT pr.id
-            , p.id AS player_id
+        (player_id, title_id, federation_id, ranking, elo, games, page)
+        SELECT
+            p.id AS player_id
             , dt.id AS title_id
             , df.id AS federation_id
-            , ranking
-            , elo
-            , games
-            , page
-        
-        FROM players_raw pr
-        LEFT JOIN player p ON pr.name = p.name
+            , pr.ranking
+            , pr.elo
+            , pr.games
+            , pr.page
+
+        FROM player p
+        LEFT JOIN players_raw pr
+            ON REPLACE(pr.name, "`", "'") = TRIM(CONCAT_WS(' ', p.firstname, p.lastname))
         LEFT JOIN dim_title dt ON pr.title=dt.txt
         LEFT JOIN dim_federation df ON pr.federation=df.txt
     """,    
     
     #games_data
-    #TODO
-    "dim_location": """
-        (txt, txt1)
-        SELECT DISTINCT (txt, txt1)
-        FROM (
-            SELECT 
-                site as txt
-                , site as txt1
-            FROM games_raw
-        )
-    """,
-    
-    "event": """
-        (id, name, location_id)
-    """,
-    
-################################
-    
-    "game": """(
-        id INTEGER NOT NULL PRIMARY KEY,
-        event_id INTEGER UNSIGNED NOT NULL,
-        white_player_id INTEGER UNSIGNED NOT NULL,
-        black_player_id INTEGER UNSIGNED NOT NULL,
-        result_id INTEGER UNSIGNED NOT NULL,
-        eco_id INTEGER UNSIGNED NOT NULL,
-        gamedate DATE,
-        stage VARCHAR(10),
-        whiteelo INTEGER UNSIGNED,
-        blackelo INTEGER UNSIGNED
-        )
-    """,
-    
     "dim_result": """(
-        id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        txt VARCHAR(100)
+        (txt) 
+        SELECT DISTINCT (result) AS txt
+        FROM games_raw
         )
     """,
     
     "dim_eco": """(
-        id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        txt VARCHAR(100)
+        (txt) 
+        SELECT DISTINCT (eco) AS txt
+        FROM games_raw
         )
     """,
     
-    "moves": """(
-        id INTEGER NOT NULL PRIMARY KEY,
-        game_id INTEGER NOT NULL,
-        movenr INTEGER UNSIGNED,
-        white VARCHAR(10),
-        black VARCHAR(10)
+    "dim_location": """(
+        (txt, txt1)
+        SELECT DISTINCT (site) AS txt, '' as txt1
+        FROM games_raw
         )
-    """
+    """,
+    
+    "event": """(
+        (name, location_id)
+        SELECT DISTINCT (gr.event) AS name
+            , dl.id AS location_id
+        FROM games_raw gr
+        LEFT JOIN dim_location dl
+            ON gr.site=dl.txt
+        )
+    """,
+    
+    "game": """(
+        (event_id, white_player_id, black_player_id, result_id, eco_id, gamedate, stage, whiteelo, blackelo, moves)
+        SELECT e.id AS event_id
+            , p1.id AS white_player_id
+            , p2.id AS black_player_id
+            , dr.id AS result_id
+            , de.id AS eco_id
+            , STR_TO_DATE(REPLACE(gr.date, "??", "01"),'%Y.%m.%d') AS gamedate
+            , gr.round AS stage
+            , gr.whiteelo
+            , gr.blackelo
+            , gr.game AS moves
+        FROM games_raw gr
+        LEFT JOIN event e
+            ON gr.event=e.name
+        LEFT JOIN player p1
+            ON SUBSTRING_INDEX(gr.white,',',-1) = p1.firstname
+            AND SUBSTRING_INDEX(gr.white,',',1) = p1.lastname
+        LEFT JOIN player p2
+            ON SUBSTRING_INDEX(gr.black,',',-1) = p2.firstname
+            AND SUBSTRING_INDEX(gr.black,',',1) = p2.lastname
+        LEFT JOIN dim_result dr
+            ON gr.result=dr.txt
+        LEFT JOIN dim_eco de
+            ON gr.eco=de.txt
+        )
+    """#,
+    #moves is not needed since    
+    #"moves": """(
+    #    (game_id, movenr, white, black)
+    #"""
 }
-
-
