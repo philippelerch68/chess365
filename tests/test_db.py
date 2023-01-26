23447f35b8a7a8f595c978c8b4725392af876a91
cuchri <cuchri_dev@outlook.com>
2023-01-26 12:23:43 +0100

    add test for table ddl

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,12 +3,21 @@ import mysql.connector
 from mysql.connector import Error
 from helpers import read_yaml
 from db.create_db import create_database
+from db.create_tables import create_tables
+from db.db_ddl import tables
 
 
 #database params
 @pytest.fixture
 def define_database_params():
-    """Returns parameters to create a test database"""
+    """Reads in the project config file and returns a list of parameters to create a test database
+    
+    Args:
+        yaml_file: yaml file that contains the project config parameters
+    
+    Returns:
+        list: list of database parameters
+    """
     config = read_yaml("config.yaml")
     db_host=config.get('DATABASE').get('db_host')
     db_database='pytest_db'
@@ -19,7 +28,23 @@ def define_database_params():
     return db
 
 
+#database tables
+@pytest.fixture
+def define_tables():
+    """Reads the database table definitions from the project
+
+    Returns:
+        dict: dictionary with project table definitions
+    """
+    return tables
+
+
 def test_mysql(define_database_params):
+    """Test connection to mysql instance
+
+    Args:
+        define_database_params: pytest.fixture function that returns a list with connection parameters
+    """
     #connect to mysql instance
     db = define_database_params
     try:
@@ -33,9 +58,14 @@ def test_mysql(define_database_params):
             connection.close()
 
     assert test_connection == True
-    
+
 
 def test_create_database(define_database_params):
+    """Test to create a database pytest_db on the mysql instance
+
+    Args:
+        define_database_params: pytest.fixture function that returns a list with connection parameters
+    """
     db = define_database_params
     #create database
     create_database(host=db[0], database=db[1], user=db[2], password=db[3])
@@ -52,3 +82,47 @@ def test_create_database(define_database_params):
             connection.close()
 
     assert test_connection == True
+
+
+def test_create_tables(define_database_params, define_tables):
+    """Test to create tables in a database pytest_db on the mysql instance
+
+    Args:
+        define_database_params: pytest.fixture function that returns a list with connection parameters
+        define_tables: pytest.fixture function that returns a dictionary wit table definitions
+    """
+    db = define_database_params
+    tables_dict = define_tables
+    
+    #create database
+    create_database(host=db[0], database=db[1], user=db[2], password=db[3])
+    
+    #create tables
+    create_tables(tables_dict, host=db[0], database=db[1], user=db[2], password=db[3])
+    
+    #test if tables are created
+    try:
+        connection = mysql.connector.connect(host=db[0], database=db[1], user=db[2], password=db[3])
+        cursor = connection.cursor()
+        cursor.execute("Show tables;")
+        db_tables = cursor.fetchall()
+        cursor.close()
+        connection.close()      
+    except mysql.connector.Error as error:
+        db_tables = []
+    finally:
+        if connection.is_connected():
+            connection.close()
+
+    assert (('dim_eco',) in db_tables) == True
+    assert (('dim_federation',) in db_tables) == True
+    assert (('dim_location',) in db_tables) == True
+    assert (('dim_result',) in db_tables) == True
+    assert (('dim_title',) in db_tables) == True
+    assert (('event',) in db_tables) == True
+    assert (('game',) in db_tables) == True
+    assert (('games_raw',) in db_tables) == True
+    assert (('moves',) in db_tables) == True
+    assert (('player',) in db_tables) == True
+    assert (('playerdetails',) in db_tables) == True
+    assert (('players_raw',) in db_tables) == True
