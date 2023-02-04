5f840ec72e2e5e9829f7c79c87e0c30268417bba
Philippe LERCH <philippe.lerch68@gmail.com>
2023-02-04 20:27:08 +0100

    streamlit add

---
+++ streamlit/main.py
@@ -0,0 +1,36 @@
+# Philippe LERCH
+
+'''
+philippe@DDev:/Projects/Datascientest/Examens/chess365/chess_4/chess365/streamlit$ streamlit run streamlit_app.py 
+Network URL: http://192.168.0.35:8501
+'''
+
+import streamlit as st
+
+st.set_page_config(
+        page_title="Chess",
+        layout="centered",
+    )
+
+
+# Side bar
+
+st.markdown("# Welcome ")
+st.sidebar.markdown("# Main page ")
+
+# Main container
+
+# Default
+text = '''
+This part is an analytics application base on a dataengineering pipeline, which loads chess data of 200 Players and their Gamehistory. 
+\n
+More info at : https://github.com/philippelerch68/chess365
+
+
+'''
+
+st.subheader("General info")
+st.markdown(text)
+        
+        
+
---
+++ streamlit/pages/game_view.py
@@ -0,0 +1,133 @@
+import streamlit as st
+import mysql.connector
+
+import io
+import chess.pgn
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+input_id = None
+game_id = None
+
+
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+    
+def select_game():
+       
+    s =[]
+    sql ='''
+       SELECT game.id, event_id, event.name, dim_location.txt as location,dim_location.txt1 as country, gamedate, plw.lastname as wlastname,plw.firstname as wfirstname ,plb.lastname as plastname,plb.firstname as pfirstname, stage,whiteelo,blackelo, dim_result.txt as result, moves FROM game
+        inner join player as plw on plw.id = white_player_id
+        inner join player as plb on plb.id = black_player_id
+        inner join event on event.id = game.event_id
+        inner join dim_result on dim_result.id = game.result_id
+        inner join dim_location on dim_location.id = event.location_id
+        where moves like "%{%" and  dim_location.txt like "%Paris%"
+        order by game.id
+        '''
+    result= run_query(sql)
+    list_value = [(row[0], row[6]+ "  <--> " + row[8]) for row in result]
+    
+    return list_value
+    
+       
+def display_chess_game(id):
+    
+    input_id = id
+    game_id = id
+    sql =f"SELECT game.id, game FROM Datascientest.game where id = {id} "
+    result= run_query(sql)
+    line=''
+    comment = False
+    a=0
+    
+    for raw in result:
+        game_id = raw[0]
+        game_value = raw[1]
+        pgn = io.StringIO(game_value)
+        game = chess.pgn.read_game(pgn)
+        game.mainline()  
+        board = game.board()
+        array=[]
+        # print(game.mainline())  see original data
+        col1, col2 = st.columns([3, 2])
+        
+        with col1:
+            display_move(id=id)
+        
+        for move in game.mainline():
+           comment = move.comment
+           if(comment != ''):
+               with col1:
+                st.subheader("Comment from game ")
+                st.write(f" {comment}")
+        a=0
+        for move in game.mainline_moves():
+            board.push(move) 
+            array.append(board.unicode())
+            a+=1
+        with col1:
+            st.subheader(f"Total numbers of battle : {a}")
+        b =0
+        with col2:
+            st.subheader("Moves")
+            for move in array:
+                b+=1
+                st.write(f"battle : {b}") 
+                st.text(move)
+        
+
+def display_move(id):
+    
+    
+    sql =f'''
+    SELECT game.id,event_id, event.name, dim_location.txt as location,dim_location.txt1 as country, gamedate, plw.lastname as wlastname,plw.firstname as wfirstname ,plb.lastname as plastname,plb.firstname as pfirstname, stage,whiteelo,blackelo, dim_result.txt as result, moves FROM game
+    inner join player as plw on plw.id = white_player_id
+    inner join player as plb on plb.id = black_player_id
+    inner join event on event.id = game.event_id
+    inner join dim_result on dim_result.id = game.result_id
+    inner join dim_location on dim_location.id = event.location_id
+    where game.id = {id}
+    ''' 
+    result= run_query(sql)
+    for raw in result:
+        st.subheader(f"Event")
+        st.write(f"{raw[5]}  {raw[2]}  at: {raw[3]} {raw[4]}")
+        st.subheader(f"Players")
+        st.markdown(f" White : **{raw[6]}** {raw[7]}  ")
+        st.write(f" Black : **{raw[8]}**  {raw[9]}  ")
+        st.subheader(f"Result ")
+        st.write(f" {raw[13]}")
+        #st.write(f" Ref:{raw[0]}")
+    
+    input_id = id
+    game_id = id
+       
+
+list_value = select_game()
+
+st.sidebar.subheader(f" Global ")   
+input_id = st.sidebar.text_input("game id (6483)")
+if input_id:
+    st.write(f" {input_id}")
+    display_chess_game(id=input_id)  #6483
+
+
+st.sidebar.subheader("Game in Paris ")  
+game_id = st.sidebar.selectbox(label='Select players',options=list_value,format_func=lambda x: x[1])
+if game_id:
+    #st.write(f" {game_id[0]}")
+    input_id = game_id[0]
+    display_chess_game(id=game_id[0])  #6483
+
+
+
+
