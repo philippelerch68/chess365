f596a663e143d7e5f9f51c2be64f6cff738bf0d5
Philippe LERCH <philippe.lerch68@gmail.com>
2023-02-19 16:30:05 +0100

    update

--- streamlit/main.py
+++ streamlit/main.py
@@ -47,11 +47,9 @@ def  get_data(sql, issue):
 
 df, result = get_data(sql ="SELECT title, data1 FROM app_stat", issue='')
      
-
+st.image("./images/chess-banner.png")
 # Default
 text = f'''
-<h1> CHESS</h1>
-<i>Analytics Engineering Project </i><br><br>
 <b>The aim of this project is to build a data engineering pipeline in the form of an ELT process that loads chess data for 200 players and their game histories. The data is loaded, fitted, and integrated into a data model built on a SQL database schema (MySQL).
 <br><i>More details can be found at  <a href="https://github.com/philippelerch68/chess365" target="_blank">Chess Project</a></i>
 <br><br><b>The last part of project is to create an analytics application. <i>(Done with Streamlit)</i>.</b>
@@ -64,7 +62,7 @@ text = f'''
 <h4> Importation information</h4>
 
 Of {result[6][1]} records, {result[5][1]} are successfully integrated on dedicated tables.
-Less than <b  style="color:Green;">1%</b> are lost ! ({result[0][1]}).<br><br>
+Less than <b  style="color:Yellow;">1%</b> are lost ! ({result[0][1]}).<br><br>
 <B>Below additional information</B><br><i>(Table record, games & players informations)<i>
 
 '''
