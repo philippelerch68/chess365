71e5fe5020f37539301554534e49c1e398c63960
Philippe LERCH <philippe.lerch68@gmail.com>
2023-02-19 12:52:00 +0100

    update

--- streamlit/pages/game.py
+++ streamlit/pages/game.py
@@ -7,21 +7,21 @@ import io
 import mysql.connector
 import streamlit as st
 from PIL import Image
-
 import base64
 import textwrap
 
-
-
 #@st.cache_resource
 def init_connection():
+    """_summary_
+        BD initialisation
+    Returns:
+        _type_: Connection
+    """
     return mysql.connector.connect(**st.secrets["mysql"])
 
 conn = init_connection()
-input_id = 1
-move_id =1
-game_id = None
-#arguments = sys.argv
+
+#arguments = sys.argv  ! Check if need for your os
 #pgnfilename = str(arguments[1])
 
 def max_width():
@@ -32,11 +32,6 @@ def max_width():
     .block-container {{
         {max_width_str}
         }}
-    
-  
- 
-    
-   
     <style>
     """,
         unsafe_allow_html=True,
@@ -44,68 +39,61 @@ def max_width():
     
 
 max_width()
-tabs_font_css = """
-<style>
-div[class*="stText"] label {
-  font-size: 26px;
-  color: red;
-}
 
-div[class*="stTextInput"] label {
-  size: 26px;
-  color: red;
-}
-
-div[class*="stNumberInput"] label {
-  font-size: 26px;
-  color: green;
-}
-
-div[class*="stWriteExpander"] label {
-  font-size: 26px;
-  color: green;
-}
-
- .streamlit-expanderHeader.st-ae.st-dj.st-ag.st-ah.st-ai.st-aj.st-bv.st-dk.st-bw.st-dl.st-dm.st-dn.st-do.st-ar.st-as.st-dp.st-dq.st-b3.st-cj.st-c5.st-dr.st-b4.st-ds.st-c3.st-c4.st-c2.st-c1{
-                           font-size:30px;
-                           text-align:center;     
-                           color:blue;
-                           background-color :red;
-                    }
-
-
-
-</style>
-"""
-
-st.write(tabs_font_css, unsafe_allow_html=True)
+st.sidebar.markdown('''
+                    This is an example who load a game board and display result.
+                    ''')
 
 tab1, tab2 = st.tabs(["Players", "Game"])
 
 def run_query(query):
+    """_summary_
+
+    Args:
+        query (_type_): Run sql query
+
+    Returns:
+        _type_: return result of query
+    """
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
 
 def render_svg(svg):
-    """Renders the given svg string."""
+    """Renders the given svg string to PNG"""
     b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
     html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
     with st.container():
         st.write(html, unsafe_allow_html=True)
+        
+        
 
 def render_svg_example(image):
+    """_summary_
+
+    Args:
+        image (_type_): Generate SVG file
+    """
     svg = image
     render_svg(svg)
+    
+    
 
 def export_svg(id):
+    """_summary_
+
+    Args:
+        id (INT): Id from game table
+
+    Returns:
+        INT: return number of moves
+    """
     
     sql =f"SELECT game.id, game.moves FROM game where id ={id}"
     result=run_query(sql)
-    
-
     for raw in result:
+            #Load data from query and generate chess game boards for each move
             a = 0
             game_id = raw[0]
             game_value = raw[1]
@@ -114,11 +102,16 @@ def export_svg(id):
             path_data = f"./images/game/{game_id}"
             isExist = os.path.exists(path_data)
             if isExist == False :
+                
                 try:
-                    mode = 0o755
-                    os.mkdir(f"./images/game/{game_id}", mode)
-                except:
-                    print("Not able to create directory !")
+                    oldmask = os.umask(000)
+                    mode = 0o777
+                    os.mkdir(path_data, mode)
+                    os.umask(oldmask)
+                except OSError as error:
+                    print(f" Please check right to create image ! {error}")
+                    exit()
+            
             try:
                 # game.mainline()  
                 board = game.board()
@@ -129,8 +122,7 @@ def export_svg(id):
                     f = open(f"./images/game/{game_id}/chess_id_{game_id}_{a}.SVG", "w")
                     f.write(boardsvg)
                     f.close()
-                
-                
+        
             except:
                 print("error")
     return a
@@ -142,36 +134,7 @@ def display_game(id):
     Args:
         id (int): id passed to DB
         
-        0 G.id 
-        1 Event 
-        2 Site
-        ------ White info
-        3 WTitle
-        4 White_player
-        5 WFederation
-        6 WRank
-        7 WElo
-        8 WPage
-        
-        -----  Black info         
-        9 BTitle
-        10 Black_player
-        11 BFederation
-        12 BRank
-        13 BElo
-        14 BPage
-        
-        ----- Game info 
-        15 Result
-        16 Eco
-        17 Game_date
-        18 Stage
-        19 Whiteelo
-        20 Blackelo
-        21 Moves
-        22 Total_move
     """
-   
     sql =f'''SELECT * FROM datascientest.cnt_Fgame where id = {id}
     ''' 
     result= run_query(sql)
@@ -184,9 +147,7 @@ def display_game(id):
         st.sidebar.text(f"Stage : {raw[18]}")
         st.sidebar.text(f" Nbr moves : {raw[22]}")
         st.sidebar.text(f"Result : {raw[15]}")
-
-        st.header(" GAME INFORMATION ")
-        st.write("--------------------------")
+        
         st.subheader(f"WHITE")
         st.text(f"Name : {raw[3]}  {raw[4]}")
         st.text(f"Federation : {raw[5]}")
@@ -218,20 +179,14 @@ def display_game(id):
                 expander = st.expander("See Comments")
                 expander.write(f"{text}")
                 
-        
-        
-            
-
-
-input_id = st.sidebar.text_input("game id",1)
-
+input_id = st.sidebar.text_input("Enter an ID from game.",1)
 
 if input_id :
-    #display_move(id=input_id)
+    
     with tab1:
         a=export_svg(id=input_id)
         display_game(input_id)
-    #image = f"./images/game/{input_id}/chess_id_{input_id}_1.SVG"
+    
 
 if a:
     with tab2:
@@ -242,5 +197,4 @@ if move_id :
     image = f.read()
     with tab2:
         render_svg_example(image)
-   
-    
+
