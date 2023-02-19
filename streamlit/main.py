39f246ba3c131652f57f35c7f60277d5061d4f8c
Philippe LERCH <philippe.lerch68@gmail.com>
2023-02-19 18:06:06 +0100

    update

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,9 +1,3 @@
-# Philippe LERCH
-
-'''
-philippe@DDev:/Projects/Datascientest/Examens/chess365/chess_4/chess365/streamlit$ streamlit run main.py 
-Network URL: http://192.168.0.35:8501
-'''
 import streamlit as st
 import matplotlib.pyplot as plt
 import mysql.connector
@@ -12,15 +6,10 @@ import chess.pgn
 import pandas as pd
 import numpy as np
 
-
-
 st.set_page_config(
         page_title="Chess",
         layout="centered",
     )
-
-
-# Side bar
 
 
 # Initialize connection.
@@ -30,11 +19,11 @@ def init_connection():
 
 conn = init_connection()
 
-
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
+    
     
 def  get_data(sql, issue):
     st.subheader(issue)
@@ -44,7 +33,7 @@ def  get_data(sql, issue):
     pd.set_option('display.max_columns', True)
     return df, result
 
-
+# statistic info
 df, result = get_data(sql ="SELECT title, data1 FROM app_stat", issue='')
      
 st.image("./images/chess-banner.png")
@@ -55,6 +44,7 @@ text = f'''
 <br><br><b>The last part of project is to create an analytics application. <i>(Done with Streamlit)</i>.</b>
 <br><br>
 <h3> Streamlit content</h3>
+    <li> main    : General information about project,data and tables. </li>
     <li> game    : Display an example of complete chess board. </li>
     <li> insight : Somes statistics about chess Games data.</li>
 
@@ -63,13 +53,30 @@ text = f'''
 
 Of {result[6][1]} records, {result[5][1]} are successfully integrated on dedicated tables.
 Less than <b  style="color:Yellow;">1%</b> are lost ! ({result[0][1]}).<br><br>
-<B>Below additional information</B><br><i>(Table record, games & players informations)<i>
 
 '''
 
 st.markdown(text,unsafe_allow_html=True)
+
+# Samples data from Game tables
+st.write("All data are from Game table and displayed via Views tables.")
+st.write("Game table sample:")
+result = run_query("SELECT id, event_id, site_id, white_player_id, black_player_id, result_id, eco_id, gamedate, stage, whiteelo, blackelo, moves FROM game limit 10;")
+dfr = pd.DataFrame(result, columns=['id', 'event_id', 'site_id', 'white_player_id', 'black_player_id', 'result_id', 'eco_id', 'gamedate', 'stage', 'whiteelo', 'blackelo', 'moves'])
+st.dataframe(dfr)
+
+# Tables views list
+st.write("Tables views list:")
+result = run_query("Show tables like 'app%';")
+dfr = pd.DataFrame(result, columns=['Tables'])
+st.dataframe(dfr)
+
+# Additionnal information coming from statistic (get_data)
+st.write("Below additional information")
 st.dataframe(df)
 
+
+# FOOTER
 
 st.markdown(''' --------------------------------- 
             Christophe P. & Philppe L. | Analytics Engineering Project | @Datascientest 2023 
